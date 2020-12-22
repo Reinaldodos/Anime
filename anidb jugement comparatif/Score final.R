9d6ab2f74959c4add62f9279595247aa3b30c603
Reinaldo Dos Santos <reinalddos@gmail.com>
2020-12-22 23:16:49 +0100

    method jenks instead sd

--- anidb jugement comparatif/Score final.R
+++ anidb jugement comparatif/Score final.R
@@ -1,8 +1,8 @@
 
 FINAL =
   output %>%
-  SCORE_FINAL(STYLE = "sd", N = 100) %>%
-  transmute(Player, Apres = ceiling(Note / 10)) %>%
+  SCORE_FINAL(STYLE = "jenks", N = 10) %>%
+  transmute(Player, Apres = ceiling(Note / 1)) %>%
   left_join(x = data$Table) %>%
   select(Player, Apres, Avant = Rating)
 
